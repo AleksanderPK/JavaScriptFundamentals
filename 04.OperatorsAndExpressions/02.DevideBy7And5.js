481b3a15cc4189687f950a03b7e55eb056c079bc
aleksander <aleksander_k@abv.bg>
2016-07-27 19:44:54 +0300

    DevideBy7And5

---
+++ 04.OperatorsAndExpressions/02.DevideBy7And5.js
@@ -0,0 +1,17 @@
+﻿function devideBy5And7(arr) {
+
+	if (arr instanceof Array && arr.every(x => !isNaN(x) && typeof x === 'number')) {
+
+		arr.forEach(function (x) {
+			if (!(x % 5) && !(x % 7)) {
+				console.log(`${x} TRUE number`);
+			}
+			else {
+				console.log(`${x} FALSE number`);
+			}
+		})
+	}
+	else {
+		return 'array is INcorrect';
+	}
+}
