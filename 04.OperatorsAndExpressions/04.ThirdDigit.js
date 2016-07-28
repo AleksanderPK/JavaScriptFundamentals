d1bebcf8b8d6323ef41d7cf1da3691ed878d0236
aleksander <aleksander_k@abv.bg>
2016-07-28 19:30:21 +0300

    ThirdDigit

---
+++ 04.OperatorsAndExpressions/04.ThirdDigit.js
@@ -0,0 +1,9 @@
+﻿function verifyThirdDigit(arr) {    
+    if ((arr[0] + '').length < 3) {
+        console.log('false: 0');
+    }
+    else {
+        let thirdDigit = (arr[0] + '')[(arr[0].length + '') - 3];
+        console.log(thirdDigit === 7 ? true : false + `: ${thirdDigit}`);
+    }
+}
