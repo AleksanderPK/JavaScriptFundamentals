14ac07810c0147bcbaa9e57ec086f0cae459d1fe
aleksander <aleksander_k@abv.bg>
2016-07-28 20:26:24 +0300

    ThirdBit

---
+++ 04.OperatorsAndExpressions/05.ThirdBit.js
@@ -0,0 +1,11 @@
+﻿function verifyThirdBit(arr) {
+	let digInBase2 = (arr[0] | 0).toString(2);
+	
+	if (digInBase2.length > 3) {
+		var thirdBit = digInBase2[digInBase2.length - 4];
+		console.log(digInBase2[digInBase2.length - 4]);
+	}
+	else {
+		console.log('0');
+	}
+}
