e9233f8ae7e9160074b07e7076392bab2f65a1e6
aleksander <aleksander_k@abv.bg>
2016-07-28 21:28:53 +0300

    PointInACircle

---
+++ 04.OperatorsAndExpressions/06.PointInACircle.js
@@ -0,0 +1,13 @@
+﻿function isInACircle(coordinates) {
+    if (coordinates.length === 2 && coordinates.every(x => typeof x === 'string') ) {
+        let x = +coordinates[0];
+        let y = +coordinates[1];
+        let distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)).toFixed(2);
+        if (distance <= 2) {
+            console.log(`yes ${distance}`);
+        }
+        else {
+            console.log(`no ${distance}`);
+        }
+    }
+}
