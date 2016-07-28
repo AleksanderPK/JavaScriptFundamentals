d772ba74ae6dfe862577a29c4d37f4df41f32fb6
aleksander <aleksander_k@abv.bg>
2016-07-28 17:30:56 +0300

    Rectangles

---
+++ 04.OperatorsAndExpressions/03.Rectangles.js
@@ -0,0 +1,12 @@
+﻿function calculatePerimeterAndArea(arr) { 
+    if (arr instanceof Array && arr.length === 2 && arr.every(x => !isNaN(x))) {
+        let width = +arr[0];
+        let height = +arr[1];
+        let perimeter = 2 * (width + height);
+        let area = width * height;
+        console.log(perimeter.toFixed(2) + ' ' + area.toFixed(2));
+    }
+    else {
+        console.log('incorect input');
+    }
+}
