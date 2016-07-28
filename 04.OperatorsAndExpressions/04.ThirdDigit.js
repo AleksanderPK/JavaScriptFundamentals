a3c77e43b71c86ecfbb8a43f36e85fee1fa76fc0
Aleksander Kochev <aleksander_k@abv.bg>
2016-07-28 19:45:30 +0300

    Delete 04.ThirdDigit.js

--- 04.OperatorsAndExpressions/04.ThirdDigit.js
+++
@@ -1,9 +0,0 @@
-﻿function verifyThirdDigit(arr) {    
-    if ((arr[0] + '').length < 3) {
-        console.log('false: 0');
-    }
-    else {
-        let thirdDigit = (arr[0] + '')[(arr[0].length + '') - 3];
-        console.log(thirdDigit === 7 ? true : false + `: ${thirdDigit}`);
-    }
-}
