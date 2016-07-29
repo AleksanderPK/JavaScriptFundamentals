9a1989ee156bd7caab45e8005adcd5aed4d5b426
aleksander <aleksander_k@abv.bg>
2016-07-29 17:51:08 +0300

    PrimeCheck

---
+++ 04.OperatorsAndExpressions/07.PrimeCheck.js
@@ -0,0 +1,19 @@
+﻿function isPrime(args) {
+    let digit = +args[0];
+    if (digit > 0 && digit < 100) {
+        if (digit === 2 || digit === 3) {
+            console.log('true');
+            return;
+        };
+        for (var devisor = (Math.sqrt(digit) | 0); devisor < digit; devisor++) {
+            if (digit % devisor === 0) {
+                console.log('false');
+                return;
+            }
+        }
+        console.log('true');
+    }
+    else {
+        console.log('incorect input');
+    }
+}
