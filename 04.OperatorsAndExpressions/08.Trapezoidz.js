121de587e31e91a885ad314b598ea60cca1b52d6
aleksander <aleksander_k@abv.bg>
2016-07-29 21:42:08 +0300

    Trapezoidz

---
+++ 04.OperatorsAndExpressions/08.Trapezoidz.js
@@ -0,0 +1,8 @@
+﻿function calculateArea(args) { 
+    if (Array.isArray(args) && args.length === 3) {
+        let a = +args[0],
+            b = +args[1],
+            h = +args[2];
+        console.log((((a + b) * h) / 2).toFixed(7));
+    }
+}
