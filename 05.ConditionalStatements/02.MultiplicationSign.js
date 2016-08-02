917d6db2541ff56811dfeb413ae8a1fe8259d169
aleksander <aleksander_k@abv.bg>
2016-08-02 18:38:54 +0300

    MultiplicationSign

---
+++ 05.ConditionalStatements/02.MultiplicationSign.js
@@ -0,0 +1,17 @@
+﻿function verifyTheSign(args) {
+	var a = +args[0],
+		b = +args[1],
+		c = +args[2];
+	if ((a > 0 && b > 0 && c > 0) || 
+		(a < 0 && b < 0 && c > 0) || 
+		(a > 0 && b < 0 && c < 0) ||
+		(a < 0 && b > 0 && c < 0)) {
+		console.log('+');
+	}
+	else if(a === 0 || b === 0 || c === 0) {
+		console.log('0');
+	}
+	else {
+		console.log('-');
+	}
+}
