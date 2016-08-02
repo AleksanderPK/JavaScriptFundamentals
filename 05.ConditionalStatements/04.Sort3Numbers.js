7db4515b935985388a9414ef4fb7d58554479d2f
aleksander <aleksander_k@abv.bg>
2016-08-02 19:57:12 +0300

    Sort3Numbers

---
+++ 05.ConditionalStatements/04.Sort3Numbers.js
@@ -0,0 +1,22 @@
+﻿function sort(args) {		
+	var a = +args[0],
+		b = +args[1],
+		c = +args[2],
+		temp;	
+	if (a < b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a < c) {
+		temp = a;
+		a = c;
+		c = temp;
+	}
+	if (b < c) {
+		temp = b;
+		b = c;
+		c = temp;
+	}
+	console.log(a + ' ' + b + ' ' + c);
+}
