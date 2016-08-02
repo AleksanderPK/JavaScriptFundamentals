a0d69705214545444432a743b543e8fecaaedb47
aleksander <aleksander_k@abv.bg>
2016-08-02 20:20:41 +0300

    DigitAsWord

---
+++ 05.ConditionalStatements/05.DigitAsWord.js
@@ -0,0 +1,4 @@
+﻿function digitAsWord(args) {    
+    var words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    console.log(!!words[args[0]] ? words[args[0]] : 'not a digit');
+}
