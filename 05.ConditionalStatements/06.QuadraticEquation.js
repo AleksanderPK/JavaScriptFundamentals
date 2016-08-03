ad1ab74cbf7ddf4a23aeb779913b504d49a0ec41
aleksander <aleksander_k@abv.bg>
2016-08-03 18:52:38 +0300

    QuadraticEquation

---
+++ 05.ConditionalStatements/06.QuadraticEquation.js
@@ -0,0 +1,24 @@
+﻿function findRoots(args) {
+
+	let a = +args[0],
+		b = +args[1],
+		c = +args[2],
+		d,
+		x1,
+		x2;
+
+	d = b * b - 4 * a * c;
+
+	if (d > 0) {
+		x1 = ((-b - Math.sqrt(d)) / (2 * a)).toFixed(2);
+		x2 = ((-b + Math.sqrt(d)) / (2 * a)).toFixed(2);
+		console.log(`x1=${x1}; x2=${x2}`);
+	}
+	else if(d === 0) {
+		x1 = -b / (2 * a).toFixed(2);
+		console.log(`x1=x2=${x1}`);
+	}
+	else {
+		console.log('no real roots');
+	}
+}
