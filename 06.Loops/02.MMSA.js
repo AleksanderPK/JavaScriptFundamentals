c829a241c2e4799102efcf50da5b3b88b98bbe75
aleksander <aleksander_k@abv.bg>
2016-08-12 20:19:17 +0300

    Min,Max,Sum,Average

--- 06.Loops/02.MMSA.js
+++ 06.Loops/02.MMSA.js
@@ -5,14 +5,12 @@
         average = 0,
         i;
 
-    max = +arr[0];
     for (i = 1; i < arr.length; i++) {
         if (arr[i] > max) {
             max = +arr[i];
         }
     };
 
-    min = +arr[0];
     for (i = 1; i < arr.length; i++) {
         if (arr[i] < min) {
             min = +arr[i];
