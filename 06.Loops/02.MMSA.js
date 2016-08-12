e9cefd33c9d7354ea5da7002a69726818045db2e
aleksander <aleksander_k@abv.bg>
2016-08-12 20:16:56 +0300

    Min,Max,Sum,Average

---
+++ 06.Loops/02.MMSA.js
@@ -0,0 +1,33 @@
+﻿function findMMSA(arr) {
+    let max = arr[0],
+        min = arr[0],
+        sum = 0,
+        average = 0,
+        i;
+
+    max = +arr[0];
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = +arr[i];
+        }
+    };
+
+    min = +arr[0];
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = +arr[i];
+        }
+    };
+
+    for (i = 0; i < arr.length; i++) {
+        sum += +arr[i];
+    };
+
+    average = (sum / arr.length);
+
+    console.log(`min=${min.toFixed(2)}`);
+    console.log(`max=${max.toFixed(2)}`);
+    console.log(`sum=${sum.toFixed(2)}`);
+    console.log(`avg=${average.toFixed(2)}`);
+
+}
