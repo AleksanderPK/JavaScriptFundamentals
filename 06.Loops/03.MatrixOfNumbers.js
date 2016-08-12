0fe12e4b6f903a99f027c2a0435a15ac7eb85ded
aleksander <aleksander_k@abv.bg>
2016-08-12 20:41:40 +0300

    MatrixOfNumbers

---
+++ 06.Loops/03.MatrixOfNumbers.js
@@ -0,0 +1,13 @@
+﻿function printMatrix(arr) {
+	var i, j,
+		line = '',
+		count = +arr[0];
+
+	for (i = 1; i <= count; i++) {
+		for (j = i; j < i + count; j++) {
+			line += `${j} `;
+		}
+		console.log(line);
+		line = '';
+	}
+}
