d5ffeea24ee3f47e4a46073e14ac1cc783ad5196
aleksander <aleksander_k@abv.bg>
2016-09-16 19:41:01 +0300

    IncreaseArrayNumbersBy5

---
+++ 07.Arrays/01.IncreaseArrayNumbers.js
@@ -0,0 +1,14 @@
+﻿function increaseByFive(n) {
+    
+    let arr = [],
+        i = 0,
+        item;
+
+    for (i = 0; i < +n; i += 1) {
+        arr.push(i * 5);
+    }
+
+    for (item of arr) {
+        console.log(item);
+    }
+}
