0fad7b9011ce332afcbdf94815663e624af6dbe0
aleksander <aleksander_k@abv.bg>
2016-09-16 20:47:44 +0300

    LexicographicallyComparison

---
+++ 07.Arrays/02.LexicographicallyCompare.js
@@ -0,0 +1,23 @@
+﻿function compare(arr) {
+
+    if (arr[0].length === arr[1].length) {
+        for (var i = 0; i < arr[0].length; i++) {
+            if (arr[0][i] > arr[1][i]) {
+                console.log('>');
+            }
+            else if (arr[0][i] < arr[1][i]) {
+                console.log('<');
+            }
+        }
+        console.log('=');
+        return;
+    }
+
+    else if (arr[0].length >= arr[1].length) {
+        console.log('>')
+    }
+
+    else if (arr[0].length <= arr[1].length) {
+        console.log('<')
+    }
+}
