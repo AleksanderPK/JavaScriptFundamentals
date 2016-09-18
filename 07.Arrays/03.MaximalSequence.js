24b2c29fec6839b715c9a3de1008621d6419a0e1
aleksander <aleksander_k@abv.bg>
2016-09-18 08:58:40 +0300

    MaximalSequence

---
+++ 07.Arrays/03.MaximalSequence.js
@@ -0,0 +1,19 @@
+﻿function findMaximalSequnce(arr) {
+
+	let curCount = 1,
+		maxCount = 0;
+
+	for (var i = 1; i < arr.length; i++) {
+		if (+arr[i -1] === +arr[i]) {
+			curCount += 1;
+		}
+		else {
+			curCount = 1;
+		}
+		if (curCount > maxCount) {
+			maxCount = curCount;
+		}
+	}
+
+	console.log(maxCount);
+}
