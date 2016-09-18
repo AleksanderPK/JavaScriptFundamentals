06972fd34d013648647d6d9ccfa77f24bc5bf2f8
aleksander <aleksander_k@abv.bg>
2016-09-18 15:48:12 +0300

    SelectionSort

---
+++ 07.Arrays/05.SelectionSort.js
@@ -0,0 +1,24 @@
+﻿function sortArray(arr) {
+	let i,
+		j,
+		ind,
+		changeValues = false;
+
+	for (i = 0; i < arr.length; i += 1) {
+		min = +arr[i];
+		for (j = i + 1; j < arr.length; j += 1) {
+
+			if (+arr[j] < +min) {
+				min = arr[j];
+				ind = j;
+				changeValues = true;
+			}
+		}
+		if (changeValues) {
+			arr.splice(ind, 1);
+			arr.splice(i, 0, min);
+			changeValues = false;
+		}		
+	}
+	console.log(arr);
+}
